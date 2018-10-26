22aa4e5c78589108a59676e6297411cf92c556c7
HellMar <github@hellmann.me>
2018-10-26 23:41:57 +0200

    * removed curl output

--- build_all_lede.sh
+++ build_all_lede.sh
@@ -90,7 +90,7 @@ function notify () {
                 curl --max-time 10 -d "chat_id=$TELEGRAM_NOTIFY_CHATID&text=$MESSAGE" $TELEGRAM_NOTIFY_URL
         fi
         if [ ! -z "$ZULIP_NOTIFY_URL" ]; then
-		curl $ZULIP_NOTIFY_URL -u $(cat ZULIP_AUTH_TOKEN) -d "type=stream" -d "to=Firmware Log" -d "subject=Log" -d "content=$MESSAGE"
+		curl --silent --output /dev/null $ZULIP_NOTIFY_URL -u $(cat ZULIP_AUTH_TOKEN) -d "type=stream" -d "to=Firmware Log" -d "subject=Log" -d "content=$MESSAGE" 2> /dev/null
         fi
 }
 
