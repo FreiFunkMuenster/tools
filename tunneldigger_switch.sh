1543dbd4635bf16137fb50221d4ed43cec195ca4
Simon Wüllhorst <mail@simon-wuellhorst.de>
2019-01-24 00:43:08 +0100

    initial version of tunneldigger_switch.sh

---
+++ tunneldigger_switch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+alt=$(uci show tunneldigger|grep broker|grep enabled)
+alt=${alt:33:1}
+echo $alt
+ 
+change_if_necessary () {
+    if [ "$alt" != "$1" ]
+    then
+        echo "Umschalten"
+        uci set tunneldigger.@broker[0].enabled="$1"
+        /etc/init.d/tunneldigger restart
+    else
+        echo "Nichts zu tun"   
+    fi
+}
+ 
+count=$(batctl o | grep ^$(batctl gwl | grep -oE "=> [^ ]+" | grep -oE "[a-f0-9\:]+" || echo offline) | grep -o "\].*" | grep -oE "\(2[0-9]{2}\)" | wc -l)
+if [[ $count -lt 1 ]]
+then
+    echo "VPN ein"
+    change_if_necessary 1
+elif [[ $count -gt 1 ]]
+then
+    echo "VPN aus"
+    change_if_necessary 0
+else
+    echo "Tue nichts"
+fi
